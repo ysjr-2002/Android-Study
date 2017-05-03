f8e999ad9122a9f670dfaf695ff1b56e83993a2e
ysj <ysjr-2002@163.com>
2017-05-04 07:01:53 +0800

    ok

--- Android-Demo/Android-Demo/FrameActivity.cs
+++ Android-Demo/Android-Demo/FrameActivity.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace Android_Demo
 {
-    [Activity(Label = "FrameActivity")]
+    [Activity(Label = "FrameActivity", MainLauncher = false)]
     public class FrameActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -20,6 +20,29 @@ namespace Android_Demo
             base.OnCreate(savedInstanceState);
             // Create your application here
             this.SetContentView(Resource.Layout.frame);
+
+            var btn = base.FindViewById<Button>(Resource.Id.btnhome);
+            btn.Click += delegate
+            {
+                Intent intent = new Intent();
+                intent.SetAction(Intent.ActionMain);
+                intent.AddCategory(Intent.CategoryHome);
+                StartActivity(intent);
+            };
+
+            var btn1 = base.FindViewById<Button>(Resource.Id.btnopenurl);
+            btn1.Click += delegate
+            {
+                Intent intent = new Intent();
+                intent.SetAction(Intent.ActionView);
+                //intent.AddCategory(Intent.CategoryHome);
+                intent.SetData(Android.Net.Uri.Parse("http://www.baidu.com"));
+                StartActivity(intent);
+            };
+        }
+
+        private void Start()
+        {
         }
     }
 }
