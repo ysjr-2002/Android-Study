917d310912dda7f2ca653803c4ee33a5a574433a
ysj <ysjr-2002@163.com>
2017-06-02 17:41:35 +0800

    ok

---
+++ FaceVisualExt/FaceVisualExt/DifferentDisplay.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FaceVisualExt
+{
+    public class DifferentDislay : Presentation
+    {
+        public DifferentDislay(Context outerContext, Display display) : base(outerContext, display)
+        {
+
+        }
+
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+        }        
+    }
+}
---
+++ FaceVisualExt/FaceVisualExt/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware.Display;
+
+namespace FaceVisualExt
+{
+    [Activity(Label = "FaceVisualExt", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            this.RequestWindowFeature(WindowFeatures.NoTitle);
+            this.Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+            SetContentView(Resource.Layout.Main);
+            //GetDisplay();
+        }
+
+        private void GetDisplay()
+        {
+            DisplayManager displayManager;//屏幕管理类
+            Display[] displays;//屏幕数组
+            displayManager = (DisplayManager)this.GetSystemService(Context.DisplayService);
+            displays = displayManager.GetDisplays();
+            //主屏
+            var display1 = displays[0];
+            if (displays.Length >= 2)
+            {
+                //副屏
+                var display2 = displays[1];
+            }
+
+            DifferentDislay anotherDisplay = new DifferentDislay(this, display1);
+            //mPresentation.getWindow().setType(
+            //WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            //mPresentation.Window.SetType(WindowManagerLayoutParams)
+            anotherDisplay.Window.SetType(WindowManagerTypes.SystemAlert);
+            anotherDisplay.Show();
+
+            //AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            //builder.SetTitle("你好");
+            //builder.SetView(Resource.Layout.Main);
+            //var dialog = builder.Create();
+            //dialog.Show();
+        }
+    }
+}
+
