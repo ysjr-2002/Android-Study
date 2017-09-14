117b96d665d22a1dcfa993254349952af9ea52e1
ysj <ysjr-2002@163.com>
2017-09-14 18:17:14 +0800

    ok

---
+++ AppSO/AppSO/data/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace AppSO.data
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Photo { get; set; }
+    }
+}
